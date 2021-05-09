a04c68d76ced18ff9776ce24a0abfb42d6a47ad2
minaiy.talati <minaiy.talati@NAG1-LHP-N07128>
2021-05-09 16:44:33 +0530

    added submit button and handled input in AddProductForm.jsx

--- Auxilium_UI/auxilium_ui/src/Components/AddProductForm/AddProductForm.jsx
+++ Auxilium_UI/auxilium_ui/src/Components/AddProductForm/AddProductForm.jsx
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import './AddProductForm.css'
-import { Container, Form } from 'react-bootstrap'
+import { Container, Form, Button } from 'react-bootstrap'
 
 const GST_Services = ['Ambulabce', 'Hospital beds', 'Medical supplies', 'Oxygen']
 
 
 const AddProductForm = () => {
     const [productType, setProductType] = useState('reselect')
+    const [verificationNumber, setVerificationNumber] = useState('')
+    const [productDesc, setProductDesc] = useState('')
+    
     const handleDropdown = (e) => {
         setProductType(e.target.value);
     }
     useEffect(() => {
-        // if (productType == 'reselect') {
-        //     alert("Select an option.")
-        // }
     }, [productType])
+
+    const handleVerificationCode = (e) => {
+        setVerificationNumber(e.target.value);
+    }
+    useEffect(() => {
+    }, [verificationNumber])
+
+    const handleProductType = (e) => {
+        setProductDesc(e.target.value);
+    }
+    useEffect(() => {
+        console.log(productDesc);
+    }, [productDesc])
 
     return (
         <Container className="Form-box">
@@ -45,14 +58,20 @@ const AddProductForm = () => {
                             <Form.Label>
                                 Food license number
                             </Form.Label>
-                            <Form.Control type="text" placeholder="Enter food license number" />
+                            <Form.Control type="text" placeholder="Enter food license number" onChange={handleVerificationCode} maxLength={14}/>
                         </Form.Group>
                         <br />
                         <Form.Group>
                             <Form.Label>
                                 {productType} description
                             </Form.Label>
-                            <Form.Control as="textarea" rows={3} />
+                            <Form.Control as="textarea" rows={3} onChange={handleProductType} maxLength={500} placeholder='Enter description' />
+                        </Form.Group>
+                        <br />
+                        <Form.Group>
+                            <Button variant="primary" type="submit">
+                                Add product
+                            </Button>
                         </Form.Group>
                     </div>
                 }
@@ -63,14 +82,20 @@ const AddProductForm = () => {
                             <Form.Label>
                                 GST number
                             </Form.Label>
-                            <Form.Control type="text" placeholder="Enter GST number" />
+                            <Form.Control type="text" placeholder="Enter GST number" onChange={handleVerificationCode} maxLength={15}/>
                         </Form.Group>
                         <br />
                         <Form.Group>
                             <Form.Label>
                                 {productType} description
                             </Form.Label>
-                            <Form.Control as="textarea" rows={3} maxLength={500} placeholder='Enter description'/>
+                            <Form.Control as="textarea" rows={3} onChange={handleProductType} maxLength={500} placeholder='Enter description' />
+                        </Form.Group>
+                        <br />
+                        <Form.Group>
+                            <Button variant="primary" type="submit">
+                                Add product
+                            </Button>
                         </Form.Group>
                     </div>
                 }
